527b3884324955726600871edfef79755213972b
Daniel Milner <danielmmilner@gmail.com>
2018-11-21 20:04:16 +1100

    Added the ability to swap ships

--- Assets/PlayerHandler.cs
+++ Assets/PlayerHandler.cs
@@ -3,6 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerHandler : MonoBehaviour {
+    private KeyCode[] keyCodes = {
+         KeyCode.Alpha1,
+         KeyCode.Alpha2,
+         KeyCode.Alpha3,
+         KeyCode.Alpha4,
+         KeyCode.Alpha5,
+         KeyCode.Alpha6,
+         KeyCode.Alpha7,
+         KeyCode.Alpha8,
+         KeyCode.Alpha9,
+     };
+
+    public GameObject[] PlayerShips;
 
     public GameObject CurrentPlayerShip;
     private CameraController cameraController;
@@ -10,6 +23,31 @@ public class PlayerHandler : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         cameraController = gameObject.GetComponent<CameraController>();
+        cameraController.SetCameraTarget(CurrentPlayerShip.transform, CurrentPlayerShip.GetComponent<CameraZoomSettings>());
+        CurrentPlayerShip.GetComponent<FlightController>().enabled = true;
+    }
+
+    void Update()
+    {
+        if (Input.GetButton("ChangeShip"))
+        {
+            for (int i = 0; i < keyCodes.Length; i++)
+            {
+                if (Input.GetKeyDown(keyCodes[i]))
+                {
+                    ChangePlayerShip(i);
+                }
+            }
+        }
+    }
+
+    private void ChangePlayerShip(int shipNumber)
+    {
+        if (shipNumber > PlayerShips.Length) { return; }
+
+        CurrentPlayerShip.GetComponent<FlightController>().enabled = false;
+
+        CurrentPlayerShip = PlayerShips[shipNumber];
         cameraController.SetCameraTarget(CurrentPlayerShip.transform, CurrentPlayerShip.GetComponent<CameraZoomSettings>());
         CurrentPlayerShip.GetComponent<FlightController>().enabled = true;
     }
