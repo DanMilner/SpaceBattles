af5e051f7b3a1db14a0be2438b01068e7ac98c43
Daniel Milner <danielmmilner@gmail.com>
2018-11-21 13:39:41 +1100

    Add a turret to fighter ship and added the ability to shoot

--- Assets/WeaponController.cs
+++ Assets/WeaponController.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 
 public class WeaponController : MonoBehaviour {
     public float missileSpeed = 50.0f;
-
     public GameObject[] Missiles;
-    private int missilesLeft;
 
+    public GameObject bulletPrefab;
+    public GameObject bulletSpawn;
+    public float bulletSpeed = 100.0f;
+    public float fireRate = 2.0f;
+    public float bulletLifeSpan = 10.0f;
+    private float bulletCooldown;
+
+    private int missilesLeft;
     private Rigidbody ShipRigidbody;
 
     // Use this for initialization
     void Start () {
         missilesLeft = Missiles.Length - 1;
         ShipRigidbody = gameObject.GetComponent<Rigidbody>();
+        bulletCooldown = fireRate;
     }
 
     // Update is called once per frame
@@ -21,6 +28,16 @@ public class WeaponController : MonoBehaviour {
         if (Input.GetMouseButtonDown(0))
         {
             if (missilesLeft >= 0) { ShootMissile(); }
+        }
+
+        bulletCooldown -= Time.deltaTime;
+
+        if (Input.GetMouseButton(0))
+        {
+            if (bulletCooldown < 0) {
+                ShootBullet();
+                bulletCooldown = fireRate;
+            }
         }
     }
 
@@ -31,5 +48,15 @@ public class WeaponController : MonoBehaviour {
         missile.GetComponent<Rigidbody>().velocity = ShipRigidbody.velocity;
         missile.GetComponent<Rigidbody>().AddForce(missile.transform.forward * missileSpeed);
         missile.GetComponentInChildren<MissileController>().enabled = true;
+    }
+
+    private void ShootBullet()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, bulletSpawn.transform.position, bulletSpawn.transform.rotation);
+        bullet.transform.parent = null;
+        bullet.GetComponent<Rigidbody>().velocity = ShipRigidbody.velocity;
+        bullet.GetComponent<Rigidbody>().AddForce(bullet.transform.forward * bulletSpeed);
+
+        Destroy(bullet, bulletLifeSpan);
     }
 }
