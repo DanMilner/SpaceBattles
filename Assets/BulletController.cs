fe91cf2950a954999ed6127326874c9b4804557e
Daniel Milner <danielmmilner@gmail.com>
2018-11-22 11:46:48 +1100

    Added Bullet controller and collider to the bullet

---
+++ Assets/BulletController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour {
+    public float lifeTime = 10.0f;
+	
+	// Update is called once per frame
+	void Update () {
+        lifeTime -= Time.deltaTime;
+
+        if (lifeTime < 0)
+        {
+            GameObject.Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter()
+    {
+        GameObject.Destroy(gameObject);
+    }
+}
